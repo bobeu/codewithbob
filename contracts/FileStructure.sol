cb7dde4893baf8341e0c09e14a7d30411a5f5e2f
Quatre.Finance <35783747+bobeu@users.noreply.github.com>
2022-10-17 12:28:34 +0100

    Initial commit

---
+++ contracts/FileStructure.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier : GNU
+
+pragma solidity 0.8.17;
+
+contract FileStructure {
+  error FileNotFound(uint actual);
+  event FileCreated(uint createdFile);
+
+  enum Folder { FOLDER1, FOLDER2 }
+  enum File { FILE1, FILE2, FILE3 }
+
+  address public owner;
+
+  bool canCreateFile;
+
+  uint counter;
+
+  struct MyCustomData {
+    bytes32 fileIntegrity;
+  }
+
+  mapping (uint=>Folder) public folders;
+
+  constructor() public {}
+
+
+  function createFile() public returns (bool) {}
+
+}
+
+
+
+
